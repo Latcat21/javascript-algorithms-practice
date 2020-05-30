268c4c39e403f2fa5942ce4368c98b804f36c702
Brian <Brian@Brians-MacBook-Air.local>
2020-05-30 15:51:08 -0500

    fizzbuzz solved

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 function addTwo(num1, num2){
   return num1 + num2;
 }
-// console.log(addTwo(2, 4))
+// console.log(addTwo(2, 4)) 6
 
 ///////// Celsiut to Fahrenheit /////////////////////////
 
@@ -11,4 +11,25 @@ function tempConvert(cel){
   let temp = cel * (9 / 5) + 32;
   return temp
 }
-console.log(tempConvert(30))
+// console.log(tempConvert(30)) 86
+
+///////// Fizz Buzz /////////////////////////
+
+function fizzBuzz(){
+  for(let i = 1 ; i < 101 ; i++){
+   
+    if( i % 15 === 0){
+      console.log('fizzbuxx')
+    }
+    else if( i % 3 === 0){
+     console.log('fizz')
+    }
+    else if( i % 5 === 0){
+      console.log('buzz')
+    }else{
+    console.log(i)
+    }
+  }
+}
+
+console.log(fizzBuzz())
