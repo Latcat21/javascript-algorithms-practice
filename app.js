0a54e05b81c878a52995be906ed4ce1476ed77b0
Brian <Brian@Brians-MacBook-Air.local>
2020-05-30 15:39:09 -0500

    celius to fahrenheit solved

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-/// Add Two numbers
+///////// Add Two numbers /////////////////////////
 
 function addTwo(num1, num2){
   return num1 + num2;
 }
+// console.log(addTwo(2, 4))
 
-console.log(addTwo(2, 4))
+///////// Celsiut to Fahrenheit /////////////////////////
+
+function tempConvert(cel){
+  let temp = cel * (9 / 5) + 32;
+  return temp
+}
+console.log(tempConvert(30))
